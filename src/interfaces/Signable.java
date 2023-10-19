6622081bcda8bc2a47be9f0f4b453b8732b13c16
Irati Garzón <127838239+iratigarzon@users.noreply.github.com>
2023-10-19 10:23:59 +0200

    Modification in the Signable interface

--- src/interfaces/Signable.java
+++ src/interfaces/Signable.java
@@ -3,13 +3,33 @@ package interfaces;
 import exceptions.*;
 import models.User;
 
+
 /**
- *
- * @author Nerea
+ * The Signable interface defines methods for signUp and signIn.
+ * @author Nerea and Irati
  */
 public interface Signable {
-
-    public User signUp(User user) throws ServerErrorException;
     
-    public User signIn(User user) throws ServerErrorException, LoginCredentialException;
+    /**
+     * Sign up a user with the provided user information.
+     *
+     * @param user The User object containing user details for registration.
+     * @return A User object representing the registered user.
+     * @throws ServerErrorException If there is a server error during the sign-up process.
+     * @throws EmailExistsException If the provided email address already exists in the database.
+     * @throws DatabaseErrorException If there is an error with the database during user registration.
+     */
+    public User signUp(User user) throws ServerErrorException, EmailExitsException, DatabaseErrorException;
+    
+    /**
+     * Sign in a user with the provided user credentials.
+     *
+     * @param user The User object containing user credentials for sign-in.
+     * @return A User object representing the signed-in user.
+     * @throws ServerErrorException If there is a server error during the sign-in process.
+     * @throws LoginCredentialException If the provided login credentials are invalid.
+     * @throws DatabaseErrorException If there is an error with the database during user sign-in.
+     */
+
+    public User signIn(User user) throws ServerErrorException, LoginCredentialException, DatabaseErrorException;
 }
