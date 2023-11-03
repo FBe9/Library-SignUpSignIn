0f31f1f6ea35a08f6b9aaf7f5fb63d6db25972fa
salineroalex <alexsalineronegro@gmail.com>
2023-11-03 10:35:36 +0100

    Rewriting of Javadoc for proper documentation

--- src/models/Privilege.java
+++ src/models/Privilege.java
@@ -1,12 +1,14 @@
 package models;
 
 /**
- * The Privilage enum defines different privilege levels that can be assigned to users in the application.
- * It includes two privilege levels: USER and ADMIN.
+ * The Privilege enum defines different privilege levels that can be assigned to
+ * users in the application. It includes two privilege levels: USER and ADMIN.
  *
- * Users with 'USER' privilege have limited access, while users with 'ADMIN' have all the privileges.
+ * Users with 'USER' privilege have limited access, while users with 'ADMIN'
+ * have all the privileges.
  *
- * @author Nerea and Irati
+ * @author Nerea
+ * @author Irati
  */
 public enum Privilege {
     USER,
